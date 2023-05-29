0ea888305a8df670a6b9560ad57fea4157839c76
mochkholil89 <50905876+mochkholil89@users.noreply.github.com>
2023-05-29 11:46:18 +0700

    Add files via upload

---
+++ Scripts/Gameover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Gameover : MonoBehaviour {
+
+	float timer = 0;
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 2) {
+			
+		}
+	}
+}
